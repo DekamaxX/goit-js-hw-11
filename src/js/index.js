9c1b52cd5a176976651875a465fd8a5a02d4f545
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-02-29 19:54:58 +0100

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.6.min.css';
 import axios from 'axios';
 //test
 const moreBtn = document.querySelector('.load-more');
